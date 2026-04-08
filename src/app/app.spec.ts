721b60f6b09a4d76c0185626d6ba538e6f73f9b5
Alberto Caparrós Trigo <acaparros87@gmail.com>
2026-04-08 21:07:28 +0200

    test(app): mock SwUpdate and enable router testing for root

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -2,21 +2,39 @@ import { provideZonelessChangeDetection } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIconRegistry } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SwUpdate } from '@angular/service-worker';
 import { TranslateModule } from '@ngx-translate/core';
+import { EMPTY } from 'rxjs';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { App } from './app';
 
 describe('App Component', () => {
   let fixture: ComponentFixture<App>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [MatSlideToggleModule, TranslateModule.forRoot()],
+  const swUpdateMock = {
+    isEnabled: false,
+    versionUpdates: EMPTY,
+    checkForUpdate: () => Promise.resolve(false),
+    activateUpdate: () => Promise.resolve(void 0),
+  };
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        App,
+        MatSlideToggleModule,
+        RouterTestingModule,
+        TranslateModule.forRoot(),
+        NoopAnimationsModule,
+      ],
       providers: [
         MatIconRegistry,
         provideZonelessChangeDetection(),
         provideRouter([]),
+        { provide: SwUpdate, useValue: swUpdateMock },
       ],
     }).compileComponents();
 
