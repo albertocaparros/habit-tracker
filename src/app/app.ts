9b08a403eb636de2f0742385e818965565d31840
Alberto Caparrós Trigo <acaparros87@gmail.com>
2026-04-08 20:58:04 +0200

    refactor(app): initialize TranslateService from persisted locale

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { SettingsStore } from './core/stores/settings/settings.store';
 import { Navigation } from './shared/components/navigation/navigation';
 
 @Component({
@@ -11,16 +12,17 @@ import { Navigation } from './shared/components/navigation/navigation';
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('Habit Tracker');
 
   private readonly translate = inject(TranslateService);
   private readonly matIconReg = inject(MatIconRegistry);
+  private readonly settingsStore = inject(SettingsStore);
 
   ngOnInit(): void {
     this.matIconReg.setDefaultFontSetClass('material-symbols-outlined');
     this.translate.addLangs(['en', 'es']);
     this.translate.setFallbackLang('en');
-    this.translate.use('en');
+    this.translate.use(this.settingsStore.locale());
   }
 }
